c3bb1dccf488c44eeb2f2834e0ab1292ad5eabb1
Jay Ribakove <jrib6@yahoo.com>
2016-05-18 19:33:01 -0400

    wip

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+var PhotoGrid = React.createClass({
+
+  render() {
+    return (
+      <div className="photo-grid">
+        PHOTO GRID YEAH BOI
+      </div>
+    )
+  }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+      </div>
+    )
+  }
+});
+
+export default Single;
