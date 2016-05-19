38bcdf37023829f34ddf25b44bc7fdcd81fdd497
Jay Ribakove <jrib6@yahoo.com>
2016-05-18 22:24:19 -0400

    adding reducers and combineReducers / rootReducer

---
+++ client/reducers/posts.js
@@ -0,0 +1,8 @@
+// a reducer takes in 2 things. The action (info about what happened) and a copy of the current state
+
+function posts(state = [], action){
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
