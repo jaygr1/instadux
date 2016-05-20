0a5dd4f72674ef0895baffb1fb493885538fe4ea
Jay Ribakove <jrib6@yahoo.com>
2016-05-20 10:06:44 -0400

    wip

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,7 @@
 // a reducer takes in 2 things. The action (info about what happened) and a copy of the current state
 
 function posts(state = [], action){
+  console.log("the post will change");
   console.log(state, action);
   return state;
 }
