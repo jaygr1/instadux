74a1a3b7cac71196907a52e5210c24e8e7e28bb6
Jay Ribakove <jrib6@yahoo.com>
2016-05-21 13:50:41 -0400

    wip likes reducer working

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,7 +1,19 @@
 // a reducer takes in 2 things. The action (info about what happened) and a copy of the current state
 
 function posts(state = [], action){
-  console.log("the post will change");
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('incremnting likes');
+      const i = action.index;
+      return [
+        ...state.slice(0, i), //every post before the post we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1) //every post after the post we are updating
+      ]
+
+    default:
+      return state
+  }
   console.log(state, action);
   return state;
 }
