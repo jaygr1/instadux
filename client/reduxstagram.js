1fb687ce7fc08e0bc0f93199e06aacd0f70b26ad
Jay Ribakove <jrib6@yahoo.com>
2016-05-23 21:00:19 -0400

    adding sentry for error tracking

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -10,6 +10,14 @@ import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url).install();
+
+// cool (below) has a pop up that lets user add info that will be attached to Sentry info
+Raven.showReportDialog();
+
 //react router
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 
